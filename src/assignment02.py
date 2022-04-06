1e0bc63f0113282cd56fd7140301312b2400374c
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-04-06 19:16:08 -0400

    following paths cyclically

--- src/assignment02.py
+++ src/assignment02.py
@@ -2,11 +2,17 @@
 r'''
  Canonical: https://github.com/lduran2/trafficSimulator/src/assignment02.py
  By        : Leomar Durán <https://github.com/lduran2>
- When      : 2022-03-30t23:17Q
+ When      : 2022-04-06t19:15Q
  For       : CIS 4XXX/Introduction to Cyber-Physical Systems
- Version   : 1.2.0
+ Version   : 2.0.1
 
  CHANGELOG :
+    v2.0.1 - 2022-04-06t19:15Q
+        following paths cyclically
+
+    v2.0.0 - 2022-04-06t18:04Q
+        squared curves
+
     v1.2.0 - 2022-03-30t23:17Q
         running some cars
 
@@ -23,47 +29,26 @@ from math import copysign
 r = 70
 
 # radius of intersection
-intersection_r = 1.75
+intersection_r = 0
 
 # the roads, curves of the square track
-roads = []
-curves = []
-
-# negative or positive
-signs = (-1, 1)
-
-# for each opposite corner
-for opp in signs:
-    # for each adjacent corner
-    for adj in signs:
-        # calculate centers of intersections
-        # flat matrix
-        (cx0, cy0, cx1, cy1) = (opp*r, opp*r, -adj*opp*r, adj*opp*r)
-        # calculate distances of the intersections
-        (dx, dy) = (cx1 - cx0, cy1 - cy0)
-        # calculate box edges
-        bx0 = cx0 - copysign(intersection_r, dx)*(dx != 0)
-        bx1 = cx1 + copysign(intersection_r, dx)*(dx != 0)
-        by0 = cy0 - copysign(intersection_r, dy)*(dy != 0)
-        by1 = cy1 + copysign(intersection_r, dy)*(dy != 0)
-        # add the road
-        roads = roads + [((bx0, by0), (bx1, by1))]
-# next adj, opp
+roads = [
+    ((-r - intersection_r, -r), (+r + intersection_r, -r)),
+    ((+r, -r - intersection_r), (+r, +r + intersection_r)),
+    ((+r + intersection_r, +r), (-r - intersection_r, +r)),
+    ((-r, +r + intersection_r), (-r, -r - intersection_r))
+]
 
 vehicle_data = {
     r'vehicle_rate': 50,
     r'vehicles': [
-        [1, {r'path': [3, 2]}],
-        [1, {r'path': [0]}],
-        [1, {r'path': [1]}]
-    ]
+        [1, {r'path': [0, 1, 2, 3, 0]}]
+    ] * 50
 }
 
 # print the data so far
 print('===roads===')
 print(roads)
-print('===curves===')
-print(curves)
 print('===vehicle data===')
 print(vehicle_data)
 
